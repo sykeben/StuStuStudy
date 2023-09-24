ec974b0d5f8d15d23a9eb8c15aaf969ca3076a7d
Benjamin Sykes <btsykes@purdue.edu>
2023-09-23 23:08:10 -0400

    Removed keyboard module (for ease of dev)

--- stustustudy/utils/ui.py
+++ stustustudy/utils/ui.py
@@ -1,8 +1,7 @@
 # Imports.
-import keyboard
 from ..console import console
 
 # Pause method: Waits for user to press enter.
 def pause():
     console.print("[b][magenta]Press enter to continue...[/magenta][/b]")
-    keyboard.wait("enter", suppress=True)
+    input()
